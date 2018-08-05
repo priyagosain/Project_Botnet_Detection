0d9350dd1ae22ffa6ffb5314cffe2d6843039de0
priyagosain <pr932087@dal.ca>
2018-08-05 06:13:21 -0300

    To add detection code for Bashlite & Mirai (9 classes)

---
+++ 1_LoadModel.py
@@ -0,0 +1,37 @@
+#This python code is for loading the classifier model
+# to predict the classes for new set of sample...
+# for dataset-1, use 1_Testing_dataset_Zeus_Alexa.csv file
+# for dataset-2, use 1_Testing_dataset_Bashlite_Mirai.csv file
+
+#Also, change the pickle file according to the datasets
+
+# Source: https://pythonprogramming.net/python-pickle-module-save-objects-serialization/
+
+# Import the required packages
+import pickle
+import pandas as pd
+import numpy as np
+
+# Read the Testing file for which classes are to be predicted.
+traffic_data = pd.read_csv("1_Testing_dataset_Zeus_Alexa.csv", encoding='utf-8')
+# Printing the dataswet shape
+print("Dataset Length: ", len(traffic_data))
+print("Dataset Shape: ", traffic_data.shape)
+
+# Printing the dataset obseravtions
+print(traffic_data.head())
+
+# load the model from disk
+decision_tree_pkl_filename = 'classifier_model.pkl'
+# decision_tree_pkl_filename = 'classifier_model_2.pkl'  # uncomment this line for second dataset and comment above line
+
+# Loading the saved decision tree model pickle
+decision_tree_model_pkl = open(decision_tree_pkl_filename, 'rb')
+decision_tree_model = pickle.load(decision_tree_model_pkl)
+print("Loaded Decision tree model :: ", decision_tree_model)
+
+predicted_values = decision_tree_model.predict(traffic_data)
+pred = np.savetxt('PredictedValues_zeus.csv', predicted_values, delimiter=',')
+# pred = np.savetxt('PredictedValues_zeus.csv', predicted_values, delimiter=',') # uncomment this line for second
+# dataset and comment above line
+print("Predicted Values:", predicted_values)
